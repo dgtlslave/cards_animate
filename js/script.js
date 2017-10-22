21f8aa0f77e67b2762f15318d332acdf824dd067
dgtlslave <slavko.alexandr@gmail.com>
2017-10-22 19:34:05 +0300

    card rotate function

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+
+  $(this).on('click', function() {
+      $('.card_r').toggleClass('card_r_rotate');
+      $('.card_a').toggleClass('card_a_rotate');
+  });
+
+});
+
+// '.card_r'
+// '.card_a'
+
+// <script>
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("p").toggleClass("main");
+//     });
+// });
+// </script>
